92edddae83b3a975006996fbd79b72d39546a39f
Ozan Onur TEK <ozantek@std.iyte.edu.tr>
2016-02-07 00:48:48 +0200

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,8 +1,8 @@
 public class Digits{
-public int getFactorialDigits(int n) {
+public int getFactorialDigits(int n) {//Calculates digits of factorial number
  return (n==0 || n==1)? 1: (int) Math.floor( ((n+0.5)*Math.log(n) - n + 0.5*Math.log(2*Math.PI))/Math.log(10) ) + 1;
 }
-public int getDigits(int n){
+public int getDigits(int n){//Calculates digits of non factorial number
 return (n==0 || n==1)? 1: (int) (Math.floor( Math.log10(n)))+ 1;
 }
 }
